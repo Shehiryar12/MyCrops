cea92058f531e09f5f33c536344d8ca19d6d818d
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-06-19 15:52:38 +0500

    learn react native

---
+++ Src/Constant/Fonts.js
@@ -0,0 +1,7 @@
+ export const Font={
+  
+    bold:"Poppins-Bold",
+    medium:"Poppins-Medium",
+    regular:"Poppins-Regular",
+    semiBold:"Poppins-SemiBold",
+ }
--- Src/Screens/Register.js
+++ Src/Screens/Register.js
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import { Font } from '../Constant/Fonts'
 
 export default function Register() {
   return (
     <View>
-       <Icon name="chevron-left" size={30} color="black"/>
-      <Text style={styles.create}>Create account</Text>
+      <Icon name="chevron-left" size={30} color="black"/>
+      <Text style={styles.create}>Create Account</Text>
       <Text style={styles.Sign}>Sign Up</Text>
+
+      
 
 
     </View>
@@ -17,16 +20,18 @@ export default function Register() {
 const styles = StyleSheet.create({
    create:{
         fontSize:20,
-         textAlign:'center',
-        // marginVertical:89,
-        
+        textAlign:'center',
+        marginTop:35,   
+        fontFamily:Font.bold
+
         
    },
 
    Sign:{
          color:"green",
-         fontSize:18,
+         fontSize:20,
         //  marginHorizontal:24,
+         marginLeft:15,
          
         
          
