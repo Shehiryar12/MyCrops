dcaa36df29bb09def32dc3566ad3f5ac88d82fdc
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-06-25 20:10:24 +0500

    learn bottomTabNavigation.js

--- Src/Screens/BottomTabScreen.js
+++ Src/Screens/BottomTabScreen.js
@@ -7,41 +7,85 @@ import AddPost from './AddPost';
 import Home from './Home';
 import Favourite from './Favourite';
 import { NavigationContainer } from '@react-navigation/native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Feather from 'react-native-vector-icons/Feather';
+
+
+
 
 const Bottom = createBottomTabNavigator();
 
 const BottomTabScreen = () => {
+  
   return (
+
+   
     <NavigationContainer>
-       <Bottom.Navigator>
+
+       <Bottom.Navigator screenOptions={{
+        tabBarActiveTintColor:"green",
+       }}>
+
+
       <Bottom.Screen
         name="Home"
         component={Home}
-        option={{ headerShown: false }}
+        options={{ headerShown: false,
+        tabBarIcon: ({size,color})=>{
+  
+          return <AntDesign name="home" size={24} />
+          
+        }
+                
+         }}
       />
+    
+
 
       <Bottom.Screen
         name="Search"
         component={Search}
-        option={{ headerShown: false }}
+        options={{ headerShown: false,
+          tabBarIcon:({size,color,})=>{
+               return <Feather name="search" size={24} />
+          }
+         }}
       />
 
         <Bottom.Screen
         name="AddPost"
         component={AddPost}
-        option={{ headerShown: false }}
+       options={{ headerShown: false,
+          tabBarIcon:({size,color})=>{
+               return <Fontisto name="shopping-basket-add" size={24}/>
+          }
+         }}
       />
 
        <Bottom.Screen
         name="Favorite"
         component={Favourite}
-        option={{ headerShown: false }}
+        options={{ headerShown: false,  
+          
+           tabBarIcon:({size,color})=>{
+               return <Fontisto name="favorite" size={24}/>
+          }
+          
+         }}
       />
 
        <Bottom.Screen
         name="Profile"
         component={Profile}
-        option={{ headerShown: false }}
+        options={{ headerShown: false,
+
+          tabBarIcon:({size,color})=>{
+               return <AntDesign name="profile" size={24}/>
+          }
+
+          }}
       />
        
        
@@ -50,7 +94,7 @@ const BottomTabScreen = () => {
     </NavigationContainer>
 
     
-    
+  
   );
 };
 
