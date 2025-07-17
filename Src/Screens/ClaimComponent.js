daa487b9117ce2d557479c0b3a614704b515b601
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-07-17 17:23:44 +0500

    CROPS

--- Src/Screens/ClaimComponent.js
+++ Src/Screens/ClaimComponent.js
@@ -1,16 +1,102 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import Claims from './Claims'
+import Icon from 'react-native-vector-icons/Feather'
+import { Font } from '../Constant/Fonts'
 
-const ClaimComponent = () => {
+const ClaimComponent = (props) => {
   return (
-    <View>
-      <Text>ClaimComponent</Text>
-    </View>
+     <View style={styles.container}>
+      
+          <View style={styles.innercontainer}>
+           <Text style={styles.innertext}>{props.title}</Text>
+           <Text style={styles.innertextprocess}>{props.status}</Text>
+
+      </View>
+             
+                 <Text style={styles.claimtext}>{props.description}</Text>
+       
+               <View style={styles.endingcontainer}>
+                 <TouchableOpacity  style={styles.button}>
+
+                    <Icon name="phone-call" size={15} color={"green"} />
+                    <Text style={styles.contacttext}>{props.contact}</Text>
+
+                 </TouchableOpacity>
+
+                   <Text style={styles.textstyle}>{props.Date}</Text>
+                </View> 
+                
+
+      </View>
   )
 }
 
 export default ClaimComponent
 
 const styles = StyleSheet.create({
+  
+    container:{
+        width:330,
+        height:150,
+        backgroundColor:"#EBEBEB",
+        borderRadius:9,
+        padding:14,
      
+    },
+
+      innercontainer:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    
+    innertext:{
+        fontSize:18,
+        fontFamily:Font.bold,
+    },
+    innertextprocess:{
+      color:'orange',
+      fontSize:11,
+       fontFamily:Font.medium,
+
+    },
+    claimtext:{
+        fontSize:10,
+        color:'#646464',
+       marginTop:2,
+    paddingTop:7,
+    width:190,
+    fontFamily:Font.medium,
+    },
+    button:{
+         width:125,
+         height:37,
+         borderRadius:23, 
+         borderWidth:1,
+       
+         borderColor:"#707070",
+        justifyContent:"space-between",
+        flexDirection:'row',
+        alignItems:'center',
+        paddingHorizontal:20,
+    },
+    contacttext:{
+        color:'green',
+        fontFamily:Font.medium,
+        fontSize:11,
+    },
+    endingcontainer:{
+        flexDirection:"row",
+        alignItems:'center',
+        marginTop:14,
+        justifyContent:'space-between',
+       
+        
+    },
+    textstyle:{
+        fontSize:11,
+        fontFamily:Font.medium,
+        color:"#646464",
+    }
 })
--- Src/Screens/Claims.js
+++ Src/Screens/Claims.js
@@ -1,30 +1,13 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import Icon from 'react-native-vector-icons/Feather'
+import ClaimComponent from './ClaimComponent'
 
 const Claims = () => {
   return (
-   <View style={styles.fullcontainer}>
-      <View style={styles.container}>
-      
-          <View style={styles.innercontainer}>
-           <Text style={styles.innertext}>Tobacco</Text>
-           <Text style={styles.innertextprocess}>Processing</Text>
-
-      </View>
-             
-                 <Text style={styles.claimtext}>Your Claim is processing, you should in touch with us.</Text>
-       
-               <View style={styles.endingcontainer}>
-                 <TouchableOpacity  style={styles.button}>
-                    <Icon name="phone-call" size={18} color={"green"} />
-                    <Text style={styles.contacttext}>Contact Us</Text>
-                 </TouchableOpacity>
-                   <Text style={styles.textstyle}>25 Dec, 2024</Text>
-                </View> 
-                
-
-      </View>
+   <View style={styles.fullcontainer} >
+  
+  <ClaimComponent  title="Tobaccooo" status="Processing" description="Your Claim is processing, you should in touch with use." Date="25 Dec, 2024"
+                   contact="Contact Us"/>
     
     </View>
   )
@@ -33,60 +16,11 @@ const Claims = () => {
 export default Claims
 
 const styles = StyleSheet.create({
-
-    fullcontainer:{
+          fullcontainer:{
        alignItems:'center',
         marginTop:80,
     },
-    container:{
-        width:330,
-        height:150,
-        backgroundColor:"#EBEBEB",
-        borderRadius:9,
-        padding:14,
-    },
-
-      innercontainer:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-    },
-    
-    innertext:{
-        fontSize:18,
-    },
-    innertextprocess:{
-      color:'orange',
-      fontSize:10,
-    },
-    claimtext:{
-        fontSize:11,
-        color:'#646464',
-       marginTop:2,
-    paddingTop:7,
-    width:180,
-    },
-    button:{
-         width:134,
-         height:37,
-         borderRadius:23, 
-         borderWidth:1,
-       
-         borderColor:"#707070",
-        justifyContent:"space-between",
-        flexDirection:'row',
-        alignItems:'center',
-        paddingHorizontal:16,
-    },
-    contacttext:{
-        color:'green',
-    },
-    endingcontainer:{
-        flexDirection:"row",
-        alignItems:'center',
-        marginTop:28,
-        justifyContent:'space-between',
-    },
+   
    
     
   
