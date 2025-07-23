f94bd6bc17d2bc67196abaa6adaed17df340f3ff
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-07-23 19:20:26 +0500

    Compensation

--- Src/Screens/Compensation.js
+++ Src/Screens/Compensation.js
@@ -1,31 +1,11 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import { Font } from '../Constant/Fonts';
-import Icon from 'react-native-vector-icons/Feather';
+import CompensationComponent from './CompensationComponent';
 
 const Compensation = () => {
   return (
     <View style={styles.container}>
-
-        <View style={styles.status}>
-          <Text style={styles.Namestatus}>Tobacco</Text>
-          <Text style={styles.Processstatus}>Processing</Text>
-        </View>
-
-        <View style={styles.claim}>
-          <Text style={styles.claimtext}>
-            Your Claim is processing, you should in touch with us.
-          </Text>
-        </View>
-
-      <View style={styles.contactcontainer}>
-          <TouchableOpacity style={styles.contact}>
-            <Icon name="phone-call" size={10} color={'green'} padding={5} />
-            <Text style={styles.contacttext}>Contact Us</Text>
-          </TouchableOpacity>
-
-          <Text style={styles.datetext}>25 Dec, 2024</Text>
-      </View>
+        <CompensationComponent title="Tobacco" process="Processing" date="25 Dec, 2024"/>
     </View>
   );
 };
@@ -34,63 +14,63 @@ export default Compensation;
 
 const styles = StyleSheet.create({
   container: {
-    width: 315,
-    height: 135,
-    backgroundColor: '#E0E0E0',
+    // width: 315,
+    // height: 135,
+    // backgroundColor: '#E0E0E0',
     marginTop: 80,
-    marginHorizontal: 23, //ya marginLeft
-    borderRadius: 5,
-    padding:10,
+    // marginHorizontal: 23, //ya marginLeft
+    // borderRadius: 5,
+    // padding:10,
   },
-  status: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems:'center',
-  },
+//   status: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems:'center',
+//   },
 
-  Namestatus: {
-    fontFamily: Font.semiBold,
-    fontSize: 15,
-  },
-  Processstatus: {
-    color: '#F39E1E',
-    fontFamily: Font.medium,
-    fontSize: 10,
-  },
-  claim: {
-    width: 190,
-  },
-  claimtext: {
-    fontSize: 10,
-    color: '#646464',
-    fontFamily: Font.medium,
-  },
-  contact: {
-    width: 100,
-    height: 30,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    borderColor: '#707070',
-    flexDirection: 'row',
-    borderWidth: 1,
-    backgroundColor: '#E0E0E0',
-    alignItems:'center',
-  },
-  contacttext: {
-    fontSize: 10,
-    fontFamily: Font.medium,
-    color: 'green',
-    marginLeft: 7,
-  },
-  contactcontainer: {
-    marginTop:23,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems:'center',
-  },
-  datetext: {
-    fontFamily: Font.medium,
-    fontSize: 10,
-    color: '#646464',
-  },
+//   Namestatus: {
+//     fontFamily: Font.semiBold,
+//     fontSize: 15,
+//   },
+//   Processstatus: {
+//     color: '#F39E1E',
+//     fontFamily: Font.medium,
+//     fontSize: 10,
+//   },
+//   claim: {
+//     width: 190,
+//   },
+//   claimtext: {
+//     fontSize: 10,
+//     color: '#646464',
+//     fontFamily: Font.medium,
+//   },
+//   contact: {
+//     width: 100,
+//     height: 30,
+//     backgroundColor: 'white',
+//     borderRadius: 15,
+//     borderColor: '#707070',
+//     flexDirection: 'row',
+//     borderWidth: 1,
+//     backgroundColor: '#E0E0E0',
+//     alignItems:'center',
+//   },
+//   contacttext: {
+//     fontSize: 10,
+//     fontFamily: Font.medium,
+//     color: 'green',
+//     marginLeft: 7,
+//   },
+//   contactcontainer: {
+//     marginTop:23,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems:'center',
+//   },
+//   datetext: {
+//     fontFamily: Font.medium,
+//     fontSize: 10,
+//     color: '#646464',
+//   },
 });
---
+++ Src/Screens/CompensationComponent.js
@@ -0,0 +1,122 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { Font } from '../Constant/Fonts'
+import Icon from 'react-native-vector-icons/Feather'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
+const CompensationComponent = (props) => {
+  return (
+     <View style={styles.container}>
+    
+            <View style={styles.status}>
+              <Text style={styles.Namestatus}>{props.title}</Text>
+              <Text style={styles.Processstatus}>{props.process}</Text>
+            </View>
+    
+            <View style={styles.claim}>
+              <Text style={styles.claimtext}>
+                Your Claim is processing, you should in touch with us.
+              </Text>
+            </View>
+    
+          <View style={styles.contactcontainer}>
+              <TouchableOpacity style={styles.contact}>
+                <Icon name="phone-call" size={10} color={'green'} padding={5} />
+                <Text style={styles.contacttext}>Contact Us</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.payment}>
+                <MaterialIcons name="payment" size={15} color={'green'} padding={5} />
+                     <Text style={styles.paymenttext}>View Payment</Text>
+              </TouchableOpacity>
+    
+              <Text style={styles.datetext}>{props.date}</Text>
+          </View>
+        </View>
+  )
+}
+
+export default CompensationComponent
+
+const styles = StyleSheet.create({
+      container: {
+        width: 315,
+        height: 135,
+        backgroundColor: '#E0E0E0',
+       
+        marginHorizontal: 23, //ya marginLeft
+        borderRadius: 5,
+        padding:10,
+      },
+      status: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:'center',
+      },
+    
+      Namestatus: {
+        fontFamily: Font.semiBold,
+        fontSize: 15,
+      },
+      Processstatus: {
+        color: '#F39E1E',
+        fontFamily: Font.medium,
+        fontSize: 10,
+      },
+      claim: {
+        width: 190,
+      },
+      claimtext: {
+        fontSize: 10,
+        color: '#646464',
+        fontFamily: Font.medium,
+      },
+      contact: {
+        width: 100,
+        height: 30,
+        backgroundColor: 'white',
+        borderRadius: 15,
+        borderColor: '#707070',
+        flexDirection: 'row',
+        borderWidth: 1,
+        backgroundColor: '#E0E0E0',
+        alignItems:'center',
+      },
+      contacttext: {
+        fontSize: 10,
+        fontFamily: Font.medium,
+        color: 'green',
+        marginLeft: 7,
+      },
+      contactcontainer: {
+        marginTop:23,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:'center',
+      },
+      datetext: {
+        fontFamily: Font.medium,
+        fontSize: 10,
+        color: '#646464',
+      },
+      paymenttext:{
+
+        fontSize: 10,
+        fontFamily: Font.medium,
+        color: 'green',
+      
+        alignItems:'center',
+        justifyContent:'center',
+      
+      },
+      payment:{
+        backgroundColor:'lightgrey',
+        width:105,
+        height:30,
+        borderRadius: 15,
+        borderColor: '#707070',
+        flexDirection:'row',
+        alignItems:'center',
+     
+      }
+})
