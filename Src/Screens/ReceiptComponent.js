e18b305e52feaf5cbd13ea2e3c916a9d010c2d03
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-07-24 19:32:20 +0500

    Receipt

--- Src/Screens/ReceiptComponent.js
+++ Src/Screens/ReceiptComponent.js
@@ -1,31 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Font } from '../Constant/Fonts'
 
-const ReceiptComponent = () => {
-  return (
-    <View>
-       <View style={styles.container}>
-              <Text style={styles.headingtext}>Khan Fertilizer</Text>
-      
-            <View style={styles.Products}>
-              <Text style={[styles.producttext,{width:150}]}>Product</Text>
-              <Text style={[styles.producttext,{width:50,textAlign:'center'}]}>Items</Text>
-              <Text style={[styles.producttext,{width:80,textAlign:'right'}]}>Price(Rs)</Text>
-            </View>
-      
+const ReceiptComponent = (props) => {
+
+    const TextComp=(props)=>{
+        return(
             <View style={styles.Products}>
               <Text style={[styles.Descriptiontext,{width:150}]}>
-                Stick Class Dispersing and adhesive
+                Stick Class Dispersing and adhesive 
               </Text>
-              <Text  style={[styles.Descriptiontext,{width:50,textAlign:'center'}]}>2</Text>
-              <Text  style={[styles.Descriptiontext,{width:80,textAlign:'right'}]}>5600</Text>    
+              <Text  style={[styles.Descriptiontext,{width:50,textAlign:'center'}]}>{props.item}</Text>
+              <Text  style={[styles.Descriptiontext,{width:80,textAlign:'right'}]}>{props.price}</Text>    
+            </View>
+            
+
+        )
+    }
+  return (
+    <View>
+           <View style={styles.container}>
+              <Text style={styles.headingtext}>{props.title}</Text>
+      
+            <View style={styles.Products}>
+                
+              <Text style={[styles.producttext,{width:150}]}>{'Product'}</Text>
+              <Text style={[styles.producttext,{width:50,textAlign:'center'}]}> {"item"}</Text>
+              <Text style={[styles.producttext,{width:80,textAlign:'right'}]}>{"Price(Rs)"}</Text>
             </View>
       
-            <View style={styles.Productstwo}> 
-                   <Text style={[styles.linetwo,{width:150}]}>Strong plus High phosphor that increase</Text>
-                   <Text style={[styles.linetwo,{width:50,textAlign:'center'}]}>2</Text>
-                   <Text style={[styles.linetwo,{width:80,textAlign:'right'}]}>5200</Text>
-            </View>
+         
+          <TextComp product="Stick Class Dispersing and adhesive" item="3" price="5600"/>
+          <TextComp product="Strong plus High phosphor that increase" item="2" price="5200"/>
+          
+          <View style={styles.dot}/>
+                        
+        
+                 
           </View>
     </View>
   )
@@ -33,4 +44,53 @@ const ReceiptComponent = () => {
 
 export default ReceiptComponent
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+     container: {
+        width: 315,
+        height: 480,
+        backgroundColor: '#24282E',
+        marginTop: 110,
+        marginHorizontal: 25, //ya marginLeft:10
+        borderRadius: 8,
+        padding:12,
+      },
+      headingtext: {
+        color: '#FFFFFF',
+        fontSize: 17,
+        fontFamily: Font.medium,
+        textAlign: 'center',
+      },
+      Products: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      producttext: {
+        color: '#86BC41',
+        fontSize: 14,
+        fontFamily: Font.semiBold,
+        marginTop:10,
+      },
+      Descriptiontext: {
+        color: '#DBD8D8',
+        fontSize: 10,
+        fontFamily: Font.regular,
+        marginTop:7,
+      },
+      Productstwo:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+      },
+    
+    linetwo:{
+        color:"#DBD8D8",
+         fontSize: 10,
+    },
+    dot:{
+       borderBottomColor: '#AAAAAA',   
+       borderBottomWidth: 2,
+       width: '100%',
+       marginTop:100,                  
+     
+       
+    }
+})
