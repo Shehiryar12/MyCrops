3562d639aa80104a264d997b2834e7d96ef0836f
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-07-27 17:02:57 +0500

    Submit

--- Src/Submit.js
+++ Src/Submit.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { Font } from './Constant/Fonts';
@@ -22,18 +22,35 @@ const Submit = () => {
 
       <View style={styles.box}>
         <View style={styles.imagepackage}>
-           <Image source={require("../Src/Screens/grassone.jpg")} style={styles.grassstyle}/>
-           <Image source={require("../Src/Screens/grasstwo.jpg")} style={styles.grassstyle}/>
-           <Image source={require("../Src/Screens/grassthree.jpg")} style={styles.grassstyle}/>  
-           <Image source={require("../Src/Screens/grassfour.jpg")} style={styles.grassing}/>
-           <Image source={require("../Src/Screens/grassfive.jpg")} style={styles.grassing}/> 
-         <View style={styles.outerimage}> 
-           <MaterialIcons name="keyboard-arrow-left" size={14} style={styles.innerimage}/>
-         </View>   
-          </View>     
-     
-        
-        
+          <Image
+            source={require('../Src/Screens/grassone.jpg')}
+            style={styles.grassstyle}
+          />
+          <Image
+            source={require('../Src/Screens/grasstwo.jpg')}
+            style={styles.grassstyle}
+          />
+          <Image
+            source={require('../Src/Screens/grassthree.jpg')}
+            style={styles.grassstyle}
+          />
+          <Image
+            source={require('../Src/Screens/grassfour.jpg')}
+            style={styles.grassing}
+          />
+          <Image
+            source={require('../Src/Screens/grassfive.jpg')}
+            style={styles.grassing}
+          />
+          <View style={styles.outerimage}>
+            <MaterialIcons name="add" size={30} style={styles.innerimage} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.button}>
+        <TouchableOpacity>
+          <Text style={styles.submitclaim}>Submit Claim</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -76,40 +93,59 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: Font.semiBold,
   },
-  box:{
-    width:317,
-    height:223,
+  box: {
+    width: 317,
+    height: 218,
+    borderRadius: 13,
+    marginTop: 20,
+    marginLeft: 26,
+    borderWidth: 1,
+    borderColor: '#DBD8D8',
+  },
+  imagepackage: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 2,
+    paddingTop: 10,
+    paddingRight: 5,
+    paddingLeft: 5,
+    flexWrap: 'wrap',
+  },
+  grassstyle: {
+    width: 95,
+    height: 95,
     borderRadius:13,
-    marginTop:20,
-    marginLeft:26,
-    borderWidth:1,
-    borderColor:"#DBD8D8",
   
   },
-    imagepackage:{
-    flexDirection:"row",
-    justifyContent:'space-between',
-    paddingHorizontal:2,
-    paddingTop:10,
-    paddingRight:5,
-    paddingLeft:5,
-    flexWrap:"wrap",
+  grassing: {
+    width: 95,
+    height: 95,
+      borderRadius: 13,
+      marginTop:5,
   },
-  grassstyle:{
-     width:95,
-     height:95,
-     borderRadius:13,   
+  outerimage: {
+    width: 95,
+    height: 95,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#DBD8D8',
+    justifyContent: 'center',
+    marginTop:5,
+    // textAlign:"center",
+    alignItems: 'center',
   },
-  grassing:{
-    width:95,
-    height:95,  
-    
-  },
-  outerimage:{
-    width:100,
-    height:100,
-    borderRadius:13,
-    borderWidth:1,
-    borderColor:"#DBD8D8",
-  }
+ button:{
+  backgroundColor:"#009245",
+  marginTop:200,
+  marginHorizontal:25,
+  padding:9,
+  borderRadius:7,
+  // width:400
+ },
+ submitclaim:{
+   textAlign:'center',
+   color:"white",
+   fontFamily:Font.semiBold,
+   fontSize:13,
+ }
 });
