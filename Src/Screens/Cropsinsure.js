74b14b48761af425d77a52b80678679819baad5a
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-07-30 20:55:51 +0500

    CROPSINSURE

--- Src/Screens/Cropsinsure.js
+++ Src/Screens/Cropsinsure.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import AppHeader from '../Components/AppHeader';
 import CropInput from '../Components/CropInput';
